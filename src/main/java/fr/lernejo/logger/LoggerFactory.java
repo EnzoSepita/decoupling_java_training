cce232cd51c94bd6c7f526fa3e3584b537ff8e34
Enzo Souane <enzo.souane@epita.fr>
2022-03-10 11:16:32 +0100

    creation d'un Logger

---
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -0,0 +1,14 @@
+package fr.lernejo.logger;
+
+public class LoggerFactory implements Logger {
+    public static Logger getLogger(String name){
+        Logger temp = null;
+
+        return temp;
+    }
+
+    @Override
+    public void log(String message) {
+
+    }
+}
