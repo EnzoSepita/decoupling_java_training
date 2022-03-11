063a8ceab25c71dac811feedc3d4ac3cfef5ae59
Enzo Souane <enzo.souane@epita.fr>
2022-03-11 16:05:22 +0100

    partie 7 ok

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -4,7 +4,7 @@ public class LoggerFactory {
 
     public static Logger getLogger(String name){
 
-        Logger temp = new ContextualLogger(name, new FilteredLogger(new ConsoleLogger(), message->message.contains("Simulation"));
+        Logger temp = new ContextualLogger(name, new FilteredLogger(new ConsoleLogger(), message->message.contains("Simulation")));
 
         return temp;
     }
